e2ce2479d7115db6134da7e7a78e8107afe584c2
Gronik4 <gronweb@yandex.ru>
2025-04-05 12:22:52 +0300

    fixed last err

--- middleware/file.js
+++ middleware/file.js
@@ -2,14 +2,15 @@ const multer = require('multer');
 const { nanoid } = require('nanoid');
 
 const fileStorage = multer.diskStorage({
-  destination: function (req, file, cb) { // Настойка хранилища
-    cb(null, 'library'); // Первый параметр - ошибка, второй - директория
+  destination(req, file, cb) { // Настойка хранилища
+    cb(null, 'library'); // Первый параметр - ошибка, второй - директория для хранения
   },
-  filename: function (req, file, cb) { // Настройка именования файлов
+  filename(req, file, cb) { // Настройка именования файлов
     cb(null, `${nanoid(7)}-${file.originalname}`);
+    console.log(nanoid(7) + file.originalname);
   }
 });
- //const allowedTypes = ['image/png', 'image/jpg', 'application/pdf', 'text/txt', 'text/js']; // Разрешенные типы файлов
+ //const allowedTypes = ['image/png', 'image/jpg', 'application/pdf', 'text/plain', 'text/js']; // Разрешенные типы файлов
  /*const fileFilter = (req, file, cb) => { //Фильтр типов файлов 
    if (allowedTypes.includes(file.mimetype)) {
      cb(null, true); 
--- routes/desc-router.js
+++ routes/desc-router.js
@@ -7,7 +7,7 @@ routerDesc.post('/upload-file',
   (req, res) => {
     if(req.file) {
       const {path} = req.file;
-      console.log(`file upload ${path}`);
+      console.log(fileMulter.single('desc-text'));
       res.json({path});
     } else {
       res.status(400);
