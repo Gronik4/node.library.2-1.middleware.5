7990cf9d4f08bf85f24f2e2dae33d41f9279ca1b
Gronik4 <gronweb@yandex.ru>
2025-04-08 18:55:24 +0300

    Added checks

--- routes/desc-router.js
+++ routes/desc-router.js
@@ -2,16 +2,24 @@ const express = require('express');
 const routerDesc = express.Router();
 const fileMulter = require('../Middleware/file');
 
-routerDesc.post('/upload-file',
+routerDesc.post('/upload-file', (req, res, next)=> {
+  if(!req.files || !req.files('desc-text')) {
+    return res.status(400).json({ error: 'Файл не найден в запросе.' });
+  }
+  next();
+},
   fileMulter.single('desc-text'),
   (req, res) => {
-    if(req.file) {
-      const {path} = req.file;
-      console.log(req.file);
-      res.json({path});
-    } else {
-      res.status(400);
-      res.json('400 | Ошибка загрузки файла.');
+    try {
+      if(req.file) {
+        const {path} = req.file;
+        res.status(200).json({path})
+      } else {res.status(400).json({ error: '400 | Ошибка загрузки файла.' })}
+    } catch(err) {
+      res.status(500).json({ error: '500 | Внутреняя ошибка сервера.' })
+    };
+    (err, req, res, next)=> {
+      res.status(500).json({ error: err.message || 'Ошибка при загрузке файла.' });
     }
 });
 
